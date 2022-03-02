a2cb39501d7419383c1bd29acebec74146d2a138
min.tian <min.tian.cn@gmail.com>
2022-03-02 17:52:06 +0800

    csv reader
    Signed-off-by: min.tian <min.tian.cn@gmail.com>

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import ZChart from "./js/index.js";
 import barChartConfig from "./barChartConfig.js";
 import scatterPlotConfig from "./scatterPlotConfig.js";
+import * as d3 from "d3";
 
 window.addEventListener("DOMContentLoaded", async () => {
-  const domSelector = "#container";
-  // const dom = document.querySelector(domSelector);
   const dataFile = "./data/data.json";
   const data = await fetch(dataFile).then((res) => res.json());
+  const csvFile = "./data/test.csv";
+  const data1 = await d3.csv(csvFile);
+  console.log(data1);
 
   ZChart({
     chartType: "scatter_plot",
