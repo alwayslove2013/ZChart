0f092b400106458478351fe01d2ed6b6b40658de
min.tian <min.tian.cn@gmail.com>
2022-03-03 14:41:01 +0800

    configjs => configjson
    Signed-off-by: min.tian <min.tian.cn@gmail.com>

--- configJS2JSON.js
+++ configJS2JSON.js
@@ -16,4 +16,86 @@ import queryNodes_QPS_50m_c from "./config/queryNodes_QPS_50m.js";
 
 const jsonDir = "./config_json/";
 
-fs.writeFile(jsonDir + "NB_RT_c" + ".json", JSON.stringify(NB_RT_c), err => {});
+fs.writeFileSync(
+  jsonDir + "NB_RT_c" + ".json",
+  JSON.stringify(NB_RT_c, null, 2),
+  () => {}
+);
+
+fs.writeFileSync(
+  jsonDir + "in_it_c" + ".json",
+  JSON.stringify(in_it_c, null, 2),
+  () => {}
+);
+
+fs.writeFileSync(
+  jsonDir + "nq_RT_1m_c" + ".json",
+  JSON.stringify(nq_RT_1m_c, null, 2),
+  () => {}
+);
+
+fs.writeFileSync(
+  jsonDir + "nq_RT_50m_4qn_c" + ".json",
+  JSON.stringify(nq_RT_50m_4qn_c, null, 2),
+  () => {}
+);
+
+fs.writeFileSync(
+  jsonDir + "nq_RT_50m_8qn_c" + ".json",
+  JSON.stringify(nq_RT_50m_8qn_c, null, 2),
+  () => {}
+);
+
+fs.writeFileSync(
+  jsonDir + "nq_RT_1m_vps_c" + ".json",
+  JSON.stringify(nq_RT_1m_vps_c, null, 2),
+  () => {}
+);
+
+fs.writeFileSync(
+  jsonDir + "nq_RT_50m_4qn_vps_c" + ".json",
+  JSON.stringify(nq_RT_50m_4qn_vps_c, null, 2),
+  () => {}
+);
+
+fs.writeFileSync(
+  jsonDir + "nq_RT_50m_8qn_vps_c" + ".json",
+  JSON.stringify(nq_RT_50m_8qn_vps_c, null, 2),
+  () => {}
+);
+
+fs.writeFileSync(
+  jsonDir + "Recall_RT_sift_hnsw_c" + ".json",
+  JSON.stringify(Recall_RT_sift_hnsw_c, null, 2),
+  () => {}
+);
+
+fs.writeFileSync(
+  jsonDir + "Recall_RT_sift_ivf_flat_c" + ".json",
+  JSON.stringify(Recall_RT_sift_ivf_flat_c, null, 2),
+  () => {}
+);
+
+fs.writeFileSync(
+  jsonDir + "Recall_RT_sift_ivf_sq8_c" + ".json",
+  JSON.stringify(Recall_RT_sift_ivf_sq8_c, null, 2),
+  () => {}
+);
+
+fs.writeFileSync(
+  jsonDir + "Recall_RT_glove_ivf_flat_c" + ".json",
+  JSON.stringify(Recall_RT_glove_ivf_flat_c, null, 2),
+  () => {}
+);
+
+fs.writeFileSync(
+  jsonDir + "RT_QPS_1m_c" + ".json",
+  JSON.stringify(RT_QPS_1m_c, null, 2),
+  () => {}
+);
+
+fs.writeFileSync(
+  jsonDir + "queryNodes_QPS_50m_c" + ".json",
+  JSON.stringify(queryNodes_QPS_50m_c, null, 2),
+  () => {}
+);
